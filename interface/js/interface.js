8fef7107c06a6c4194fca27256bdb07454603897
Lucas R. Martins <lukasrms@gmail.com>
2013-05-12 15:38:45 -0300

    moving files into new repository

--- interface/js/interface.js
+++ interface/js/interface.js
@@ -4,37 +4,34 @@ var tabs = [];
 
 var Panes = {
 	createNewPane: function() {
-		var newWidth =  $("#tab-pane-"+panes.length).width() - 
-		( ($("#tab-pane-"+panes.length).width()/100) * 40 );
-	
-		$("#tab-pane-"+panes.length).animate ({ 'width': newWidth }, 'slow');
-		
 		this.addPane();
-		$("#tab-pane-"+panes.length).css('width','');
-		
 	},
-	addPane: function() {
+    createChartPane: function() {
+        this.addPane('chart');
+    },
+	addPane: function(type) {
 		// Add a new item on array
+        if (!type) { type = 'sensor'; }
 		panes.push('pane-'+ (panes.length+1));
-		
 		
 		// Creates a new pane on page and append the tab ul
 		var placeholder = $('#new-pane-placeholder');
-		$('<div class="tab-pane" id="tab-pane-'+panes.length+'"></div>').insertAfter(placeholder);
-		$('#tab-pane-'+panes.length).html(placeholder.html());
+		$('<div class="tab-pane" id="tab-pane-'+panes.length+'"></div>').insertBefore(placeholder);
+        
+        var newPane = '#tab-pane-'+panes.length;
+        var $newPane = $(newPane);
+        
+        if (type=='sensor') {
+            $newPane.html(placeholder.html());
+        } else {
+            $newPane.html(type)
+        }
 
-
-		tabs.push('#tab-pane-'+panes.length);
-		
-		$('#tab-pane-'+panes.length).resizable({
-				grid: 50
-		});
-
-	
-		this.reloadPanesUI('#tab-pane-'+panes.length);
+		tabs.push(newPane);
+		$newPane.addClass(type+'-pane').resizable({grid: 50});
+		this.reloadPanesUI(newPane);
 	},
 	reloadPanesUI: function(pane) {
-         
 		if (pane == undefined) {
 			pane = "#tab-pane-1";
 		}
